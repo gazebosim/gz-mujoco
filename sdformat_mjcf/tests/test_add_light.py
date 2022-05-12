8ca160ec6d0391b19c0ac7518cd6492c65b620c5
ahcorde <ahcorde@gmail.com>
2022-05-12 11:14:33 +0200

    make linters happy
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- sdformat_mjcf/tests/test_add_light.py
+++ sdformat_mjcf/tests/test_add_light.py
@@ -77,5 +77,6 @@ class LightTest(unittest.TestCase):
         light_mjcf = add_light(body, None, self.test_pose)
         self.assertEqual(None, light_mjcf)
 
+
 if __name__ == "__main__":
     unittest.main()
