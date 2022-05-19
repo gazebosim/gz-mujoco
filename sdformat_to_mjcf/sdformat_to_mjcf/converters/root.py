65e397a758e9394dec5d23a8cade96ae17331ce3
Addisu Z. Taddese <addisu@openrobotics.org>
2022-05-19 15:17:05 -0500

    Add cli.py and test, Move root conversion to converters/root.py
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- sdformat_to_mjcf/sdformat_to_mjcf/converters/root.py
+++ sdformat_to_mjcf/sdformat_to_mjcf/converters/root.py
@@ -1,29 +1,31 @@
 # Copyright (C) 2022 Open Source Robotics Foundation
-
+#
 # Licensed under the Apache License, version 2.0 (the "License")
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #       http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import argparse
-import os
-import sys
 
-import sdformat as sdf
 from dm_control import mjcf
-from dm_control.mjcf.export_with_assets import export_with_assets
 from sdformat_to_mjcf.converters.model import add_model
 from sdformat_to_mjcf.converters.world import add_world
 
 
-def sdformat_root_to_mjcf(root):
+def add_root(root):
+    """
+    Converts an SDFormat Root object to MJCF.
+
+    :param sdformat.Root root: The SDFormat root to be converted.
+    :return: The newly created MJCF root element.
+    :rtype: mjcf.RootElement
+    """
     mjcf_root = mjcf.RootElement()
     mjcf_root.compiler.eulerseq = 'XYZ'
     if root.model():
@@ -62,23 +64,3 @@ def sdformat_root_to_mjcf(root):
     elif root.world_count() > 0:
         # Does not support multiple worlds
         return add_world(mjcf_root, root.world_by_index(0))
-
-
-def sdformat_file_to_mjcf(model_file, output_file):
-    root = sdf.Root()
-    errors = root.load(model_file)
-    if errors:
-        print(errors, file=sys.stderr)
-    else:
-        mjcf_root = sdformat_root_to_mjcf(root)
-        export_with_assets(mjcf_root, os.path.dirname(output_file),
-                           output_file)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("model_file")
-    parser.add_argument("output_file")
-
-    args = parser.parse_args()
-    sdformat_file_to_mjcf(args.model_file, args.output_file)
