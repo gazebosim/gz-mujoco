1df8b68aac928a0790580f33993a5c8b8a81bc73
ahcorde <ahcorde@gmail.com>
2022-05-24 21:25:16 +0200

    Add feedback and default values
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- mjcf_to_sdformat/mjcf_to_sdformat/converters/light.py
+++ mjcf_to_sdformat/mjcf_to_sdformat/converters/light.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ignition.math import Color, Pose3d, Quaterniond
+from ignition.math import Angle, Color, Pose3d, Quaterniond, Vector3d
+
+import math
 
 import sdformat_mjcf_utils.sdf_utils as su
 
@@ -22,6 +24,16 @@ LIGHT_NUMBER = 0
 
 
 def mjcf_light_to_sdf(light):
+    """
+    Converts a MJCF light in an SDFormat light.
+
+    Default values are defined in the following reference:
+     - https://mujoco.readthedocs.io/en/latest/XMLreference.html
+
+    :param mjcf.Light light: The MJCF light
+    :return: The newly created SDFormat light.
+    :rtype: sdf.Light
+    """
     light_sdf = sdf.Light()
     if light.name is not None:
         light_sdf.set_name(light.name)
@@ -40,22 +52,52 @@ def mjcf_light_to_sdf(light):
         light_sdf.set_constant_attenuation_factor(light.attenuation[0])
         light_sdf.set_linear_attenuation_factor(light.attenuation[1])
         light_sdf.set_quadratic_attenuation_factor(light.attenuation[2])
+    else:
+        light_sdf.set_constant_attenuation_factor(1.0)
+        light_sdf.set_linear_attenuation_factor(0.0)
+        light_sdf.set_quadratic_attenuation_factor(0.0)
 
     if light.diffuse is not None:
         light_sdf.set_diffuse(Color(light.diffuse[0],
                                     light.diffuse[1],
                                     light.diffuse[2]))
+    else:
+        light_sdf.set_diffuse(Color(0.7, 0.7, 0.7))
+
     if light.specular is not None:
         light_sdf.set_specular(Color(light.specular[0],
                                      light.specular[1],
                                      light.specular[2]))
+    else:
+        light_sdf.set_specular(Color(0.3, 0.3, 0.3))
+
+    if light.active is not None:
+        light_sdf.set_light_on(light.active == "true")
+
+    def set_spot_light(light, light_sdf):
+        light_sdf.set_type(sdf.LightType.SPOT)
+        if light.cutoff is not None:
+            # always in degrees regardless of the global angle setting.
+            light_sdf.set_spot_inner_angle(
+                Angle(light.cutoff * math.pi / 180.0))
+        else:
+            light_sdf.set_spot_inner_angle(Angle(45 * math.pi / 180.0))
+        if light.exponent is not None:
+            light_sdf.set_spot_falloff(light.exponent)
+        else:
+            light_sdf.set_spot_falloff(10)
+
     if light.directional is not None:
         if light.directional == "true":
             light_sdf.set_type(sdf.Light.LightType.DIRECTIONAL)
         else:
-            light_sdf.set_type(sdf.Light.LightType.SPOT)
+            set_spot_light(light, light_sdf)
+    else:
+        set_spot_light(light, light_sdf)
 
     if light.dir is not None:
         light_sdf.set_direction(su.list_to_vec3d(light.dir))
+    else:
+        light_sdf.set_direction(Vector3d(0, 0, -1))
 
     return light_sdf
--- mjcf_to_sdformat/tests/test_add_mjcf_light_to_sdf.py
+++ mjcf_to_sdformat/tests/test_add_mjcf_light_to_sdf.py
@@ -18,12 +18,33 @@ from dm_control import mjcf
 
 from ignition.math import Color, Vector3d
 
+import math
+
 import sdformat as sdf
 
 from mjcf_to_sdformat.converters import light as light_conv
 
 
 class GeometryTest(unittest.TestCase):
+
+    def test_light_default(self):
+        mujoco = mjcf.RootElement(model="test")
+        body = mujoco.worldbody.add('body')
+        light = body.add('light')
+
+        sdf_light = light_conv.mjcf_light_to_sdf(light)
+        self.assertEqual("light_0", sdf_light.name())
+        self.assertEqual(Vector3d(0, 0, 0), sdf_light.raw_pose().pos())
+        self.assertEqual(1, sdf_light.constant_attenuation_factor())
+        self.assertEqual(0, sdf_light.linear_attenuation_factor())
+        self.assertEqual(0, sdf_light.quadratic_attenuation_factor())
+        self.assertEqual(Color(0.7, 0.7, 0.7), sdf_light.diffuse())
+        self.assertEqual(Color(0.3, 0.3, 0.3), sdf_light.specular())
+        self.assertEqual(sdf.LightType.SPOT, sdf_light.type())
+        self.assertEqual(Vector3d(0, 0, -1), sdf_light.direction())
+        self.assertAlmostEqual(45, sdf_light.spot_inner_angle().degree())
+        self.assertEqual(10, sdf_light.spot_falloff())
+        self.assertTrue(sdf_light.light_on())
 
     def test_light(self):
         mujoco = mjcf.RootElement(model="test")
@@ -36,7 +57,9 @@ class GeometryTest(unittest.TestCase):
                          diffuse=[0.8, 0.7, 0.6],
                          specular=[0.2, 0.2, 0.2],
                          directional="true",
-                         dir=[0, -1, -1])
+                         dir=[0, -1, -1],
+                         active="false")
+
         sdf_light = light_conv.mjcf_light_to_sdf(light)
         self.assertEqual("light_mujoco", sdf_light.name())
         self.assertEqual(Vector3d(0, 1, 2), sdf_light.raw_pose().pos())
@@ -45,8 +68,9 @@ class GeometryTest(unittest.TestCase):
         self.assertEqual(1.3, sdf_light.quadratic_attenuation_factor())
         self.assertEqual(Color(0.8, 0.7, 0.6), sdf_light.diffuse())
         self.assertEqual(Color(0.2, 0.2, 0.2), sdf_light.specular())
-        self.assertEqual(sdf.Light.LightType.DIRECTIONAL, sdf_light.type())
+        self.assertEqual(sdf.LightType.DIRECTIONAL, sdf_light.type())
         self.assertEqual(Vector3d(0, -1, -1), sdf_light.direction())
+        self.assertFalse(sdf_light.light_on())
 
     def test_light_none(self):
         mujoco = mjcf.RootElement(model="test")
@@ -59,18 +83,24 @@ class GeometryTest(unittest.TestCase):
                          diffuse=[0.8, 0.7, 0.6],
                          specular=[0.2, 0.2, 0.2],
                          directional="false",
-                         dir=[0, -1, -1])
+                         dir=[0, -1, -1],
+                         active="true",
+                         cutoff="40",
+                         exponent="5")
         sdf_light = light_conv.mjcf_light_to_sdf(light)
 
-        self.assertEqual("light_0", sdf_light.name())
+        self.assertEqual("light_1", sdf_light.name())
         self.assertEqual(Vector3d(0, 1, 2), sdf_light.raw_pose().pos())
         self.assertEqual(0.1, sdf_light.constant_attenuation_factor())
         self.assertEqual(0.3, sdf_light.linear_attenuation_factor())
         self.assertEqual(1.3, sdf_light.quadratic_attenuation_factor())
         self.assertEqual(Color(0.8, 0.7, 0.6), sdf_light.diffuse())
         self.assertEqual(Color(0.2, 0.2, 0.2), sdf_light.specular())
-        self.assertEqual(sdf.Light.LightType.SPOT, sdf_light.type())
+        self.assertEqual(sdf.LightType.SPOT, sdf_light.type())
         self.assertEqual(Vector3d(0, -1, -1), sdf_light.direction())
+        self.assertAlmostEqual(40, sdf_light.spot_inner_angle().degree())
+        self.assertEqual(5, sdf_light.spot_falloff())
+        self.assertTrue(sdf_light.light_on())
 
 
 if __name__ == "__main__":
