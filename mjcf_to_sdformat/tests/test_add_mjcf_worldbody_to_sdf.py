4ddc9651d767619561a3c62c30d321c3f56f1725
ahcorde <ahcorde@gmail.com>
2022-05-25 16:30:08 +0200

    fixed merge
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- mjcf_to_sdformat/tests/test_add_mjcf_worldbody_to_sdf.py
+++ mjcf_to_sdformat/tests/test_add_mjcf_worldbody_to_sdf.py
@@ -23,8 +23,6 @@ import sdformat_mjcf_utils.sdf_utils as su
 
 from tests.helpers import TEST_RESOURCES_DIR
 
-GeometryType = sdf.Geometry.GeometryType
-
 
 class ModelTest(unittest.TestCase):
 
@@ -105,7 +103,7 @@ class ModelTest(unittest.TestCase):
                         su.vec3d_to_list(collision_2.raw_pose().pos()))
         assert_allclose([0, 0, 0],
                         su.vec3d_to_list(collision_2.raw_pose().rot().euler()))
-        self.assertEqual(GeometryType.CAPSULE,
+        self.assertEqual(sdf.GeometryType.CAPSULE,
                          visual_2.geometry().type())
 
         link_3 = model.link_by_index(2)
