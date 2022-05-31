60f2d2162628378f78608a1af461e6baaa3addf6
Alejandro Hernández Cordero <ahcorde@gmail.com>
2022-05-31 22:04:19 +0200

    MJCF to SDF: Added gravity, wind and magnetic field (#56)
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- mjcf_to_sdformat/mjcf_to_sdformat/converters/world.py
+++ mjcf_to_sdformat/mjcf_to_sdformat/converters/world.py
@@ -12,8 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from mjcf_to_sdformat.converters.link import mjcf_body_to_sdf
+from ignition.math import Vector3d
+
 from mjcf_to_sdformat.converters.light import mjcf_light_to_sdf
+from mjcf_to_sdformat.converters.link import mjcf_body_to_sdf
 
 import sdformat_mjcf_utils.sdf_utils as su
 
@@ -48,6 +50,18 @@ def mjcf_worldbody_to_sdf(mjcf_root, physics, world):
     world.add_model(model_static)
 
     body = mjcf_root.worldbody.body
+
+    if mjcf_root.option is not None:
+        if mjcf_root.option.flag.gravity == "disable":
+            world.set_gravity(Vector3d(0, 0, 0))
+        elif mjcf_root.option.gravity is not None:
+            world.set_gravity(su.list_to_vec3d(mjcf_root.option.gravity))
+        if mjcf_root.option.magnetic is not None:
+            world.set_magnetic_field(
+                su.list_to_vec3d(mjcf_root.option.magnetic))
+        if mjcf_root.option.wind is not None:
+            world.set_wind_linear_velocity(
+                su.list_to_vec3d(mjcf_root.option.wind))
 
     def iterate_bodies(input_body, model, body_parent_name=None):
         for body in input_body:
--- mjcf_to_sdformat/tests/test_add_mjcf_worldbody_to_sdf.py
+++ mjcf_to_sdformat/tests/test_add_mjcf_worldbody_to_sdf.py
@@ -39,6 +39,11 @@ class ModelTest(unittest.TestCase):
 
         mjcf_worldbody_to_sdf(mjcf_model, physics, world)
 
+        self.assertEqual(Vector3d(0, 0, -9.8), world.gravity())
+        self.assertEqual(Vector3d(0.5645e-06, 2.28758e-05, -4.23884e-05),
+                         world.magnetic_field())
+        self.assertEqual(Vector3d(2, 1, 7.6), world.wind_linear_velocity())
+
         self.assertEqual("default", world.name())
         self.assertEqual(2, world.model_count())
         model = world.model_by_index(0)
@@ -223,6 +228,17 @@ class ModelTest(unittest.TestCase):
                         su.vec3d_to_list(light_1.raw_pose().pos()))
         assert_allclose([0, 0, 0],
                         su.vec3d_to_list(light_1.raw_pose().rot().euler()))
+
+    def test_option_no_gravity(self):
+        filename = str(TEST_RESOURCES_DIR / "test_no_gravity.xml")
+        mjcf_model = mjcf.from_path(filename)
+        physics = mujoco.Physics.from_xml_path(filename)
+
+        world = sdf.World()
+        world.set_name("default")
+
+        mjcf_worldbody_to_sdf(mjcf_model, physics, world)
+        self.assertEqual(Vector3d(0, 0, 0), world.gravity())
 
 
 if __name__ == "__main__":
