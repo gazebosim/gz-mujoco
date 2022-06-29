c3b58106161bd2d7beae9b28a028205e2e7006d6
Addisu Z. Taddese <addisu@openrobotics.org>
2022-06-29 14:40:58 -0500

    Prepare for 0.1.0
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- sdformat_mjcf/setup.py
+++ sdformat_mjcf/setup.py
@@ -1,23 +1,38 @@
 # -*- coding: utf-8 -*-
-
 from setuptools import setup, find_packages
 import pathlib
 
+
+# Adapted from https://github.com/pypa/pip/blob/main/setup.py#L15 based on the
+# guide:
+# https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+def get_version(rel_path: str) -> str:
+    for line in open(rel_path).read().splitlines():
+        if line.startswith("__version__"):
+            # __version__ = "0.9"
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
-license_content = (here / "LICENSE").read_text(encoding="utf-8")
 
 
 setup(
-    name='sdformat_mjcf',
-    version='0.0.1',
-    description='Tool to convert MJCF file to SDFormat',
+    name='sdformat_mjcf_test',
+    version=get_version('src/sdformat_mjcf/__version__.py'),
+    description='Tool to convert between SDFormat and MJCF',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Open Robotics',
     author_email='info@openrobotics.org',
     url='https://github.com/gazebosim/gz-mujoco',
-    license=license_content,
+    license='Apache License 2.0',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    python_requires='>=3.8',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     test_suite='tests',
